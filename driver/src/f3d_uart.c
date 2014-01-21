288e782ddb595cd9cccc8ced21772e66d8b26428
Bryce Himebaugh <bhimebau@gmail.com>
2014-01-21 12:30:23 -0500

    template for lab2

---
+++ driver/src/f3d_uart.c
@@ -0,0 +1,65 @@
+/* f3d_uart.c --- 
+ * 
+ * Filename: f3d_uart.c
+ * Description: 
+ * Author: Bryce Himebaugh
+ * Maintainer: 
+ * Created: Thu Sep 26 07:05:26 2013
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change log:
+ * 
+ * 
+ */
+
+/* Copyright (c) 2004-2007 The Trustees of Indiana University and 
+ * Indiana University Research and Technology Corporation.  
+ * 
+ * All rights reserved. 
+ * 
+ * Additional copyrights may follow 
+ */
+
+/* Code: */
+
+#include <stm32f30x.h>
+#include <f3d_led.h>
+
+void f3d_uart_init(void) {
+  // Initialization routines related to UART1
+
+}
+
+int putchar(int c) {
+  // 
+  //
+  //
+} 
+
+int getchar(void) {
+  //
+  //
+  //
+}
+
+void putstring(char *s) {
+  //
+  //
+  //
+}
+
+
+
+/* f3d_uart.c ends here */
---
+++ lab2/main.c
@@ -0,0 +1,51 @@
+/* main.c --- 
+ * 
+ * Filename: main.c
+ * Description: 
+ * Author: 
+ * Maintainer: 
+ * Created: Thu Jan 10 11:23:43 2013
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change log:
+ * 
+ * 
+ */
+/* Code: */
+
+#include <stm32f30x.h>  // Pull in include files for F30x standard drivers 
+#include <f3d_led.h>     // Pull in include file for the local drivers
+
+// Simple looping delay function
+void delay(void) {
+  int i = 2000000;
+  while (i-- > 0) {
+    asm("nop"); /* This stops it optimising code out */
+  }
+}
+
+int main(void) { 
+  while (1);
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+/* Infinite loop */
+/* Use GDB to find out why we're here */
+  while (1);
+}
+#endif
+
+/* main.c ends here */
