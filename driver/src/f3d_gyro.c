5a4cdc348bac313b84df86bfa04edf247ee7296a
Bryce Himebaugh <bhimebau@gmail.com>
2014-01-31 05:29:11 -0500

    added the template files for lab3

---
+++ driver/src/f3d_gyro.c
@@ -0,0 +1,77 @@
+/* f3d_gyro.c --- 
+ * 
+ * Filename: f3d_gyro.c
+ * Description: 
+ * Author: Bryce Himebaugh
+ * Maintainer: 
+ * Created: Thu Jan 24 05:43:01 2013 (-0500)
+ * Version: 
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * URL: 
+ * Doc URL: 
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* Code: */
+#include <f3d_gyro.h>
+
+void f3d_gyro_interface_init() {
+  //
+  //
+} 
+
+void f3d_gyro_init(void) {
+  //
+  //
+}
+
+void f3d_gyro_write(uint8_t* pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite) {
+  //
+  //
+}
+
+static uint8_t f3d_gyro_sendbyte(uint8_t byte) {
+  //
+  //
+}
+
+void f3d_gyro_getdata(float *pfData) {
+  //
+  //
+}
+
+
+
+
+
---
+++ lab3/main.c
@@ -0,0 +1,55 @@
+/* main.c --- 
+ * 
+ * Filename: main.c
+ * Description: 
+ * Author: 
+ * Maintainer: 
+ * Created: Thu Jan 10 11:23:43 2013
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change log:
+ * 
+ * 
+ */
+/* Code: */
+
+#include <stm32f30x.h>  // Pull in include files for F30x standard drivers 
+#include <f3d_led.h>     // Pull in include file for the local drivers
+#include <f3d_uart.h>
+#include <f3d_gyro.h>
+#include <stdio.h>
+
+#define TIMER 20000
+
+int main(void) { 
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
+
+  f3d_led_init();
+  f3d_uart_init();
+  printf("Reset\n");
+  while (1);
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+/* Infinite loop */
+/* Use GDB to find out why we're here */
+  while (1);
+}
+#endif
+
+/* main.c ends here */
