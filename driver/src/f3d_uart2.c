303494e49637e578add6b31fee480f76a8ae8c37
Varun Chandrashekhar Patil <vcpatil@cottonmouth.soic.indiana.edu>
2014-04-24 17:58:11 -0400

    Latest

--- driver/src/f3d_uart2.c
+++ driver/src/f3d_uart2.c
@@ -1,44 +1,44 @@
 #include <f3d_uart2.h>
 #include <queue.h>
-static queue_t rxbuf3;
-static queue_t txbuf3;
+static queue_t rxbuf2;
+static queue_t txbuf2;
 void f3d_uart2_init(void) {
 
   // Initialization routines related to UART1
   GPIO_InitTypeDef GPIO_InitStructure;
   USART_InitTypeDef USART_InitStructure;
   NVIC_InitTypeDef NVIC_InitStructure;
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
   GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOC,&GPIO_InitStructure);
-
-  GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-  GPIO_PinAFConfig(GPIOB,3,GPIO_AF_7);
-  GPIO_PinAFConfig(GPIOB,4,GPIO_AF_7);
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOC,10,GPIO_AF_7);
+  GPIO_PinAFConfig(GPIOC,11,GPIO_AF_7);
 
   USART_StructInit(&USART_InitStructure);
-  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_BaudRate = 57600;
   USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
   USART_Init(USART2 ,&USART_InitStructure);
   USART_Cmd(USART2 , ENABLE);
 
   // Initialize the rx and tx queues
-  init_queue(&rxbuf3);
-  init_queue(&txbuf3);
+  init_queue(&rxbuf2);
+  init_queue(&txbuf2);
   // Setup the NVIC priority and subpriority
   NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x08;
@@ -53,10 +53,10 @@ void USART2_IRQHandler(void) {
   int ch;
   if (USART_GetFlagStatus(USART2, USART_FLAG_RXNE)) {
     ch = USART_ReceiveData(USART2);
-    while (!enqueue(&rxbuf3,ch));
+    while (!enqueue(&rxbuf2,ch));
   }
   if (USART_GetFlagStatus(USART2,USART_FLAG_TXE)) {
-    ch = dequeue(&txbuf3);
+    ch = dequeue(&txbuf2);
     if (ch) {
       USART_SendData(USART2,ch);
     }
@@ -66,10 +66,13 @@ void USART2_IRQHandler(void) {
   }
 }
 
-
+/*void flush2()
+{
+  USART_ITConfig(USART2,USART_IT_TXE,ENABLE);
+}*/
 
 int putchar2(int c) {
-  while (!enqueue(&txbuf3,c));
+  while (!enqueue(&txbuf2,c));
 }
 
 /* while (USART_GetFlagStatus(USART2,USART_FLAG_TXE) ==(uint16_t)RESET) */
@@ -82,7 +85,7 @@ int putchar2(int c) {
 
 int getchar2(void) {
   int ch;
-  while (!(ch=dequeue(&rxbuf3)));
+  while (!(ch=dequeue(&rxbuf2)));
   return (ch);
 }
 
